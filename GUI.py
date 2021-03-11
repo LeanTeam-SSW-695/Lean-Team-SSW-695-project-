a0ac2b52eb75cefaa1274b2e95ed7f8ce64c268a
Chengyi Zhang <61096603+MarquisBan@users.noreply.github.com>
2021-03-11 22:58:42 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,20 +7,21 @@
 
 import tkinter
 import tkinter.messagebox
+import urllib.error
 import pickle
 import webbrowser
 import main
 
 screen = tkinter.Tk()
-screen.title("LeanMap")
+screen.title("Travel Companion")
 screen.geometry("800x600")
 
-"""
-canvas = tkinter.Canvas(screen, height= , width= )
-image_file = tkinter.PhotoImage(file="")
-image = canvas.create_image(60, 20, anchor='nw', image=image_file)
+
+canvas = tkinter.Canvas(screen, height=233, width=750)
+image_file = tkinter.PhotoImage(file="Image.gif")
+image = canvas.create_image(30, 20, anchor='nw', image=image_file)
 canvas.pack(side='top')
-"""
+
 
 tkinter.Label(screen, text='Origin Address: ').place(x=50, y=300)
 tkinter.Label(screen, text='Destination Address: ').place(x=50, y=360)
@@ -34,8 +35,8 @@ entry_destination.place(x=200, y=360)
 
 
 def reset():
-    entry_destination.select_clear()
-    entry_origin.select_clear()
+    entry_destination.delete(0, 'end')
+    entry_origin.delete(0, 'end')
 
 
 def calc():
@@ -45,8 +46,12 @@ def calc():
         answer = main.main(originAddress, destinationAddress)
         output = "Distance between origin and destination is about {} miles.".format(answer)
         tkinter.Label(screen, text=output).place(x=50, y=500)
-    except:
-        tkinter.messagebox.showerror()
+    except (ValueError, IndexError, urllib.error.URLError):
+        tkinter.messagebox.showerror(title='Error!',
+                                     message='Please make sure you enter your one-line address correctly.')
+    except urllib.error.HTTPError:
+        tkinter.messagebox.showerror(title='Error!',
+                                     message='Connection Error!')
 
 
 btn_reset = tkinter.Button(screen, text='Reset', command=reset)
