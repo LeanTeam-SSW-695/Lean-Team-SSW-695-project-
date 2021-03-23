8dbe0cd77c9d724e4e8f169fd26242d84c3d11bf
abod <ashahran@stevens.edu>
2021-03-23 15:37:18 -0400

    implementing google's distance matrix API, geocoding API and adding the current location button using Selenium

--- GUI.py
+++ GUI.py
@@ -16,12 +16,10 @@ screen = tkinter.Tk()
 screen.title("Travel Companion")
 screen.geometry("800x600")
 
-
 canvas = tkinter.Canvas(screen, height=233, width=750)
 image_file = tkinter.PhotoImage(file="Image.gif")
 image = canvas.create_image(30, 20, anchor='nw', image=image_file)
 canvas.pack(side='top')
-
 
 tkinter.Label(screen, text='Origin Address: ').place(x=50, y=300)
 tkinter.Label(screen, text='Destination Address: ').place(x=50, y=360)
@@ -39,13 +37,29 @@ def reset():
     entry_origin.delete(0, 'end')
 
 
+def getLocOrigin():
+    coordinates = main.get_location()
+    entry_origin.insert(0, coordinates['neighborhood'])
+    origin_coordinates = coordinates['lat']+coordinates['lng']
+    return origin_coordinates
+
+
+def getLocDestina():
+    coordinates = main.get_location()
+    entry_destination.insert(0, coordinates['neighborhood'])
+    destination_coordinates = coordinates['lat']+coordinates['lng']
+    return destination_coordinates
+
+
 def calc():
     originAddress = origin_address.get()
     destinationAddress = destination_address.get()
     try:
-        answer, originWeather, destinationWeather = main.main(originAddress, destinationAddress)
-        output = "Distance between origin and destination is about {} miles.\nThe temperature at destination" \
-                 " address is {}°C, and at origin is {}°C".format(answer, originWeather, destinationWeather)
+        theDistance, theDuration, originWeather, destinationWeather = main.main(originAddress, destinationAddress)
+        output = "Distance between origin and destination is about {} and the duration of the drive is {}." \
+                 "\nThe temperature at destination" \
+                 " address is {}°F, and at origin is {}°F".format(theDistance, theDuration, originWeather,
+                                                                  destinationWeather)
         tkinter.Label(screen, text=output).place(x=50, y=500)
     except (ValueError, IndexError, urllib.error.URLError):
         tkinter.messagebox.showerror(title='Error!',
@@ -57,8 +71,11 @@ def calc():
 
 btn_reset = tkinter.Button(screen, text='Reset', command=reset)
 btn_reset.place(x=300, y=460)
+btn_currLoc1 = tkinter.Button(screen, text='Current Location', command=getLocOrigin)
+btn_currLoc1.place(x=650, y=330)
+btn_currLoc2 = tkinter.Button(screen, text='Current Location', command=getLocDestina())
+btn_currLoc2.place(x=650, y=390)
 btn_calc = tkinter.Button(screen, text='Calculate', command=calc)
 btn_calc.place(x=500, y=460)
 
 screen.mainloop()
-
--- main.py
+++ main.py
@@ -11,6 +11,39 @@ import urllib.request, urllib.error, urllib.parse
 import json
 import math
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+def get_location():
+    options = Options()
+    options.add_argument("--use--fake-ui-for-media-stream")
+    driver = webdriver.Chrome(executable_path='/Users/abod/Desktop/SSW 695/LeanTeam-SSW-695/chromedriver',
+                              options=options)
+    timeout = 10
+    driver.get("https://mycurrentlocation.net/")
+    wait = WebDriverWait(driver, timeout)
+    time.sleep(3)
+    longitude = driver.find_elements_by_xpath('//*[@id="longitude"]')
+    longitude = [x.text for x in longitude]
+    longitude = str(longitude[0])
+    latitude = driver.find_elements_by_xpath('//*[@id="latitude"]')
+    latitude = [x.text for x in latitude]
+    latitude = str(latitude[0])
+    neighborhood = driver.find_elements_by_xpath('//*[@id="neighborhood"]')
+    neighborhood = [x.text for x in neighborhood]
+    neighborhood = str(neighborhood[0])
+    driver.quit()
+
+    coordinates = dict()
+    coordinates['lat'] = latitude
+    coordinates['lng'] = longitude
+    coordinates['neighborhood'] = neighborhood
+
+    return coordinates
+
 
 
 def read_address(address):
@@ -18,55 +51,54 @@ def read_address(address):
         of that address
     """
 
-    global js
-    geocoding_api = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?'
-    parms = dict()
-    parms['address'], parms['benchmark'], parms['format'] = address, 'Public_AR_Current', 'json'
+    apiKey = "AIzaSyDNOv-Nu97S_HcvAl7j4OqLsjsesmf-5TI"
+    geocoding_api = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = urllib.parse.urlencode({"address": address, "key": apiKey})
+    url = f"{geocoding_api}?{params}"
 
-    url = geocoding_api + urllib.parse.urlencode(parms)
-
-    uh = urllib.request.urlopen(url)
-    data = uh.read().decode()
-
-    js = json.loads(data)
-
+    result = json.load(urllib.request.urlopen(url))
     coordinates = dict()
+    coordinates['lat'] = result['results'][0]['geometry']['location']['lat']
+    coordinates['lng'] = result['results'][0]['geometry']['location']['lng']
 
-    coordinates['x'] = js['result']['addressMatches'][0]['coordinates']['x']
-    coordinates['y'] = js['result']['addressMatches'][0]['coordinates']['y']
+    if result["status"] in ["OK", "ZERO_RESULTS"]:
+        return coordinates
 
-    return coordinates
+    raise Exception(result["error_message"])
 
 
 def distance(user_address_1, user_address_2):
     """ a function that return the distance in miles """
 
-    x1, y1 = user_address_1['x'], user_address_1['y']
-    x2, y2 = user_address_2['x'], user_address_2['y']
+    originCoor = "%s,%s" % (user_address_1['lat'], user_address_1['lng'])
+    destinCoor = "%s,%s" % (user_address_2['lat'], user_address_2['lng'])
+    api_key = "AIzaSyDNOv-Nu97S_HcvAl7j4OqLsjsesmf-5TI"
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={}&destinations={}&key={}" \
+        .format(originCoor, destinCoor, api_key)
 
-    radius = 3956
+    response = requests.get(url)
 
-    dx = math.radians(x2 - x1)
-    dy = math.radians(y2 - y1)
+    data = json.loads(response.text)
+    ''' = data["rows"]["elements"]["duration"]["text"]'''
+    if data["status"] in ["OK"]:
+        routeDistance = data['rows'][0]['elements'][0]['distance']['text']
+        routeDuration = data['rows'][0]['elements'][0]['duration']['text']
+        return routeDistance, routeDuration
+    elif data["status"] in ["ZERO_RESULTS"]:
+        raise Exception(data['status'])
+    else:
+        raise Exception(data["error_message"])
 
-    a = math.sin(dx / 2) * math.sin(dx / 2) + math.cos(math.radians(x1)) \
-        * math.cos(math.radians(x2)) * math.sin(dy / 2) * math.sin(dy / 2)
-
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    d = radius * c
-
-    return d
 
 def the_weather(user_weather_1, user_weather_2):
     """ a function to retrieve the current temperature of both addresses using the OpenWeatherMap API"""
 
-    x1, y1 = user_weather_1['y'], user_weather_1['x']
-    x2, y2 = user_weather_2['y'], user_weather_2['x']
+    x1, y1 = user_weather_1['lat'], user_weather_1['lng']
+    x2, y2 = user_weather_2['lat'], user_weather_2['lng']
 
     api_key = "7a160b5068bd8bad7f789b5e9b450a69"
-    url_1 = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (x1, y1, api_key)
-    url_2 = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (x2, y2, api_key)
+    url_1 = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (x1, y1, api_key)
+    url_2 = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (x2, y2, api_key)
 
     response_1 = requests.get(url_1)
     response_2 = requests.get(url_2)
@@ -81,15 +113,13 @@ def the_weather(user_weather_1, user_weather_2):
 
 
 def main(address_1, address_2):
-
     user_address_1 = read_address(address_1)
     user_address_2 = read_address(address_2)
 
-    the_distance = distance(user_address_1, user_address_2)
+    the_distance, the_duration = distance(user_address_1, user_address_2)
 
     user_weather_1 = user_address_1
     user_weather_2 = user_address_2
     weather_1, weather_2 = the_weather(user_weather_1, user_weather_2)
-    
-    return round(the_distance, 2), weather_1, weather_2
 
+    return the_distance, the_duration, weather_1, weather_2
