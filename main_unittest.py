1ae2051c75c60eaff94fff8644ef9e00d8fdc531
xeal3k <xeal3k@gmail.com>
2021-04-07 23:41:56 +0800

    Update main_unittest.py

--- main_unittest.py
+++ main_unittest.py
@@ -19,7 +19,7 @@ class TestAPP(unittest.TestCase):
     def test_distance(self):
         #test distance to see whether it can get the right distance between two coordinates
         x_coordinates = {'lat': 42.338842, 'lng': -71.15455279999999}
-        y_coordinates = {'lat': 40.748558, 'lng': -73.9857578}}
+        y_coordinates = {'lat': 40.748558, 'lng': -73.9857578}
         self.assertEqual(main.distance(x_coordinates, y_coordinates), ('208 mi', '3 hours 32 mins'))
 
 if __name__ == '__main__':
